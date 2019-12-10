823672c805bd819057b4578be1956b257883e234
balayaydemir <balayaydemir@gmail.com>
2019-12-10 11:14:12 -0500

    second comit

---
+++ drills.js
@@ -0,0 +1,15 @@
+// Big O Drills! 
+
+
+/**
+ * #1
+ * 
+ * Determine the Big O for the following algorithm: You are sitting in a room with 15 people. You want to 
+ * find a playmate for your dog, preferably of the same breed. So you want to know if anyone out of the 15 
+ * people have the same breed as your dog. You stand up and yell out, who here has a golden retriever and would 
+ * like to be a playdate for my golden. Someone yells - "I do, be happy to bring him over"
+ * 
+ * 
+ * 
+ * 
+ */
